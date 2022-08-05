1408b2b7235aef6709b373981bed64b490fd8055
Cameronwood611 <cwood611@uab.edu>
2022-08-05 13:03:55 -0500

    remove django stuffs

--- PerfectAttendance/courses_api/urls.py
+++
@@ -1,9 +0,0 @@
-# from django.conf.urls import url
-from django.urls import path, include
-from .views import (
-    CourseListApiView,
-)
-
-urlpatterns = [
-    path('', CourseListApiView.as_view()),
-]
